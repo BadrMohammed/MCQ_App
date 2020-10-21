1df9ea6326c56c1c4158176ea9e8fff60a997f5b
badrmohammed <badrmohammed442@gmail.com>
2020-10-21 21:58:03 +0200

    Updates

---
+++ precache-manifest.cf1fd9ddaaa2c52fea4136aadcdae6fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d891d2021037a694b91083c92e93bd",
+    "url": "/mcq_App/index.html"
+  },
+  {
+    "revision": "e1b1d089a1a3258bcd87",
+    "url": "/mcq_App/static/css/2.b39d7127.chunk.css"
+  },
+  {
+    "revision": "9ea447cc9af09f457e20",
+    "url": "/mcq_App/static/css/main.be9860a4.chunk.css"
+  },
+  {
+    "revision": "e1b1d089a1a3258bcd87",
+    "url": "/mcq_App/static/js/2.e6846fdf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/mcq_App/static/js/2.e6846fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea447cc9af09f457e20",
+    "url": "/mcq_App/static/js/main.117a7e6c.chunk.js"
+  },
+  {
+    "revision": "f808b699309b55f7077c",
+    "url": "/mcq_App/static/js/runtime-main.48e55891.js"
+  }
+]);
